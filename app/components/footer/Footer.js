e741ec9e10d023a81909257dd9a16e92a23cee9f
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-09-15 22:20:31 +0100

    .

--- app/components/footer/Footer.js
+++ app/components/footer/Footer.js
@@ -6,13 +6,11 @@ const Footer = () => {
       <div className="footer-content position-relative">
         <div className="container">
           <div className="row">
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="footer-info">
                 <h3>Arte in Casa Tua</h3>
                 <p>
                   Liguria, Italy
-                  {/* https://maps.app.goo.gl/t9M8mfMhYmmcmv5d6 */}
-                  {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234.08265469447494!2d8.482039738858468!3d44.31155269439236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d2e1ff986e7361%3A0x24bcefc724b7e45c!2sVia%20Montegrappa%2C%206%2C%2017100%20Savona%20SV%2C%20Italy!5e1!3m2!1sen!2stn!4v1725889063007!5m2!1sen!2stn" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
                   <br />
                   <br />
                   <strong>Telefono:</strong> +39 327 945 2547
@@ -37,7 +35,7 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className="col-lg-3 col-md-3 footer-links">
+            <div className="col-lg-4 col-md-3 footer-links">
               <h4>Link utili</h4>
               <ul>
                 <li>
@@ -55,7 +53,7 @@ const Footer = () => {
               </ul>
             </div>
 
-            <div className="col-lg-3 col-md-3 footer-links">
+            <div className="col-lg-4 col-md-3 footer-links">
               <h4>Our Services</h4>
               <ul>
                 <li>
@@ -73,7 +71,7 @@ const Footer = () => {
               </ul>
             </div>
 
-            <div className="col-lg-3 col-md-6 footer-info">
+            {/* <div className="col-lg-3 col-md-6 footer-info">
               <h4>Informazioni sugli Sviluppatori</h4>
               <p>Sito web creato e progettato da:</p>
               <ul style={{ padding: "0" }}>
@@ -137,7 +135,7 @@ const Footer = () => {
                   </div>
                 </li>
               </ul>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
@@ -151,8 +149,8 @@ const Footer = () => {
             </strong>
             . Tutti i diritti riservati
           </div>
-          <div className="credits" style={{ marginTop: "10px" }}>
-            Progettato da Sviluppatori: <br />
+          <div className="credits" style={{ marginTop: "5px" }}>
+            Progettato da Sviluppatori{" "}
             <a href="https://jawharhmidi.netlify.app/">Jawhar Hmidi</a> e{" "}
             <a href="https://www.linkedin.com/in/fatenselmi/">Faten Selmi</a>
           </div>
